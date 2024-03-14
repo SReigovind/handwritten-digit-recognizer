14c55ab9f6cf164b8bbe78c76980e7644f216ccf
SReigovind <sreegovindsudarsan2003@gmail.com>
2024-03-14 18:40:11 +0530

    Define and Compile CNN Model

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 # Reshape the data: Add a channel dimension
 x_train = x_train.reshape(-1, 28, 28, 1)
 x_test = x_test.reshape(-1, 28, 28, 1)
+
+# Define the CNN model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
